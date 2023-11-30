500b389c3ad309215813b13f15de29b4acf231a1
SACHIN <sachin@SACHINs-MacBook-Pro.local>
2023-11-30 23:26:10 +0530

    added py file

---
+++ Text-to-speech.py
@@ -0,0 +1,9 @@
+from gtts import gTTS
+import os
+
+def text_to_speech(text, language='en', filename='output.mp3'):
+    tts = gTTS(text=text, lang=language, slow=False)
+    tts.save(filename)
+    os.system(f"open {filename}")
+
+text_to_speech("Welcome to BUYBAZAAR")
