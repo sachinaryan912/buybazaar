b288e287ff05f38bf946a334c6e1204e5f286e4a
sachinaryan912 <sachinsachu1998.ss@gmail.com>
2023-11-30 23:37:34 +0530

    added spring boot for backend part

---
+++ buybazaar-spring/src/main/java/com/company/buybazaar/BuybazaarApplication.java
@@ -0,0 +1,13 @@
+package com.company.buybazaar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuybazaarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuybazaarApplication.class, args);
+	}
+
+}
---
+++ buybazaar-spring/src/test/java/com/company/buybazaar/BuybazaarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.buybazaar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuybazaarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
