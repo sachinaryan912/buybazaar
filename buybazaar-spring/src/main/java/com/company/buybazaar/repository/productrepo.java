dec8e887735af2e56d2c226a109a4975de73bc52
sachinaryan912 <sachinsachu1998.ss@gmail.com>
2023-12-05 02:11:25 +0530

    spring connected to db for products

---
+++ buybazaar-spring/src/main/java/com/company/buybazaar/repository/productrepo.java
@@ -0,0 +1,9 @@
+package com.company.buybazaar.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.company.buybazaar.Entity.Product;
+
+public interface productrepo extends CrudRepository<Product,Integer>{
+
+}
